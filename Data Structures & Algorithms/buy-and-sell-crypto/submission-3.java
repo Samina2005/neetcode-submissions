b7ceba834563bfcf3348b2e6e8fe2f77c3b70f9a
Samina2005 <sultansamina25@gmail.com>
2026-05-09 04:55:43 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max =0;
+        for(int i=n-1;i>=0;i--){
+            if(prices[i]>max){
+                max = prices[i];
+                prices[i] =0;
+            }else{
+                prices[i] = max -prices[i];
+            }
+        }
+
+        int num =0;
+        for(int i:prices){
+            num = Math.max(num,i);
+        }
+        return num;
+    }
+}
